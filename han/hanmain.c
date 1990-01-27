cd6d78a65b27667bef0bc55efa8b235931a54441
Rhialto <rhialto@falu.nl>
1990-01-27 20:13:00 +0000

    *** empty log message ***

--- han/hanmain.c
+++ han/hanmain.c
@@ -1,6 +1,9 @@
 /*-
- * $Id: hanmain.c,v 1.2 89/12/17 21:24:47 Rhialto Exp Locker: Rhialto $
+ * $Id: hanmain.c,v 1.3 89/12/17 21:29:51 Rhialto Exp Locker: Rhialto $
  * $Log:	hanmain.c,v $
+ * Revision 1.3  89/12/17  21:29:51  Rhialto
+ * *** empty log message ***
+ *
  * Revision 1.1  89/12/17  20:02:31  Rhialto
  * Initial revision
  *
@@ -27,7 +30,7 @@
 
 extern char	DotDot[1 + 8 + 3];
 struct Library *IntuitionBase;
-static char RCSId = "Messydos filing system $Revision$ $Date$, by Olaf Seibert";
+static char RCSId[] = "Messydos filing system $Revision: 1.3 $ $Date: 89/12/17 21:29:51 $, by Olaf Seibert";
 
 byte
 ToUpper(ch)
