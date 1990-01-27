bdea9bde4db3fe3fa95626b3394968cce475da3c
Rhialto <rhialto@falu.nl>
1990-01-27 20:38:18 +0000

    Variable #sectors/track! (max 10)

--- dev/dev.h
+++ dev/dev.h
@@ -1,5 +1,5 @@
 /*-
- *  $Id$
+ *  $Id: dev.h,v 1.1 89/12/17 20:07:52 Rhialto Exp Locker: Rhialto $
  *
  *  Include file for users of the messydisk.device
 -*/
@@ -19,6 +19,6 @@ typedef unsigned long ulong;
  */
 
 #define MS_BPS	    512 	/* Bytes per sector */
-#define MS_SPT	    9		/* Sectors per track */
-#define MS_SPT_MAX  9		/* Max sectors per track */
+#define MS_SPT	    9		/* Default sectors per track */
+#define MS_SPT_MAX  10		/* Max sectors per track */
 #define MS_NSIDES   2		/* Tracks per cylinder */
