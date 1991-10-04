6f569ef548f0fda922b0f0e6b71592b2355edf12
Rhialto <rhialto@falu.nl>
1991-10-04 00:12:32 +0000

    Add confirmation requesters and a switch for them

--- han/hancmd.c
+++ han/hancmd.c
@@ -1,6 +1,9 @@
 /*-
- * $Id: hancmd.c,v 1.42 91/06/13 23:44:47 Rhialto Exp $
+ * $Id: hancmd.c,v 1.43 91/09/28 01:33:22 Rhialto Exp $
  * $Log:	hancmd.c,v $
+ * Revision 1.43  91/09/28  01:33:22  Rhialto
+ * DICE conversion.
+ *
  * Revision 1.42  91/06/13  23:44:47  Rhialto
  * DICE conversion
  *
@@ -19,8 +22,8 @@
  *
  * Special commands through MSH::something file names.
  *
- * This code is (C) Copyright 1990 by Olaf Seibert. All rights reserved. May
- * not be used or copied without a licence.
+ * This code is (C) Copyright 1990,1991 by Olaf Seibert. All rights reserved.
+ * May not be used or copied without a licence.
 -*/
 
 #include <amiga.h>
@@ -35,10 +38,31 @@
 #endif
 
 extern int	CheckBootBlock;
+static void	ltoa(unsigned long l, char *a);
+
+int		DoMessages = 1;
+
+static void
+ltoa(l, a)
+unsigned long	l;
+char	       *a;
+{
+    if (l > 999) {
+	a[0] = '*';
+	a[1] = '*';
+	a[2] = '*';
+    } else {
+	a[0] = '0' + l / 100;
+	l %= 100;
+	a[1] = '0' + l / 10;
+	l %= 10;
+	a[2] = '0' + l;
+    }
+}
 
 void
 HandleCommand(cmd)
-register char  *cmd;
+char	       *cmd;
 {
 #ifdef HDEBUG
     if (cmd[1] == 'D') {
@@ -46,13 +70,34 @@ register char  *cmd;
 #endif
     if (cmd[1] == 'B') {
 	CheckBootBlock = atoi(&cmd[2]);
+	if (DoMessages) {
+	    static char msg[] = "BootBlk: 000";
+
+	    ltoa(CheckBootBlock, msg + 9);
+	    DisplayMessage(msg);
+	}
     } else if (cmd[1] == 'F') {
-	if (cmd[2] == '+')
+	if (cmd[2] == '+') {
 	    DiskIOReq->iotd_Req.io_Flags |= IOMDF_40TRACKS;
-	else if (cmd[2] == '-')
+	    if (DoMessages) {
+		DisplayMessage("40 track mode");
+	    }
+	} else if (cmd[2] == '-') {
 	    DiskIOReq->iotd_Req.io_Flags &= ~IOMDF_40TRACKS;
-	else
+	    if (DoMessages) {
+		DisplayMessage("80 track mode");
+	    }
+	} else {
 	    DiskIOReq->iotd_Req.io_Flags = atoi(&cmd[2]);
+	    if (DoMessages) {
+		static char msg[] = "io_Flags: 000";
+
+		ltoa(DiskIOReq->iotd_Req.io_Flags, msg + 10);
+		DisplayMessage(msg);
+	    }
+	}
+    } else if (cmd[1] == 'M') {
+	DoMessages = atoi(&cmd[2]);
     }
 }
 
