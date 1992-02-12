5453abfbc86ede8f1969102a24d746e8f1a3cc47
Rhialto <rhialto@falu.nl>
1992-02-12 21:24:58 +0000

    New date-to-days function.

--- han/date.c
+++ han/date.c
@@ -1,9 +1,13 @@
 /*-
- * $Id: date.c,v 1.42 91/06/14 00:09:48 Rhialto Exp $
+ * $Id: date.c,v 1.46 91/10/06 18:27:01 Rhialto Rel $
  * $Log:	date.c,v $
+ * Revision 1.46  91/10/06  18:27:01  Rhialto
+ *
+ * Freeze for MAXON
+ *
  * Revision 1.42  91/06/14  00:09:48  Rhialto
  * DICE conversion
- * 
+ *
  * Revision 1.40  91/03/03  17:55:18  Rhialto
  * Freeze for MAXON
  *
@@ -29,10 +33,68 @@
 #include "han.h"
 
 #ifdef HDEBUG
-#   define	debug(x)  syslog x
+#   include "syslog.h"
 #else
 #   define	debug(x)
 #endif
+
+/*
+Article 2344 of alt.sources.d:
+Path: wn1.sci.kun.nl!sun4nl!mcsun!uunet!munnari.oz.au!yoyo.aarnet.edu.au!sirius.ucs.adelaide.edu.au!amodra
+From: amodra@ucs.adelaide.edu.au (Alan Modra)
+Newsgroups: alt.sources.d
+Subject: Re: timelocal (struct tm to time_t conversion) algorithm...
+Message-ID: <5699@sirius.ucs.adelaide.edu.au>
+Date: 20 Dec 91 03:27:53 GMT
+References: <20169@rpp386.cactus.org>
+Organization: Information Technology Division, The University of Adelaide, AUSTRALIA
+Lines: 48
+
+From article <20169@rpp386.cactus.org>, by jfh@rpp386.cactus.org (John F Haugh II):
+> While we are all trotting out our mktime.c de la dia, here is one that
+
+Thought I'd trot out my contribution too !
+
+The following routine uses a reasonably concise method to convert the
+date, which is probably the most awkward part in generating GMT since
+1/1/1970.  Feel free to tack this bit of code in whatever other sources
+you like!
+
+OIS: 2922 is original_unixdays(1978, 1, 1) so we can use it for
+     AmigaDOS dates.
+
+#include <stdlib.h>
+#include <stdio.h>
+*/
+
+
+long unixdays(int year, int month, int day)
+/* converts date to days since 1/1/1970
+   no check is made for sensible parameters
+   BTW this routine works for dates before 1/1/1970 too
+   (The Gregorian calendar only goes back to 1582 though !)
+*/
+{
+    if ( (month -= 3) < 0 ) /* 1..12 -> 10,11,0..9 */
+    {			    /* Puts Feb last since it has 28 days */
+	month += 12;	    /* Now days per month is 31,30,31,30,31, */
+	year -= 1;	    /*	(starting from Mar)  31,30,31,30,31, */
+    }			    /*			     31,28 */
+#if 0	/* for those who like calculations ! */
+    month *= 31+30+31+30+31;/* Note the pattern. */
+    day += month/5;	    /* We make use of it here and in the next line */
+    if ( month % 5 > 2 ) day += 1;
+#else	/* for those who like table look-up ! */
+    {
+	static int monthdays[12] = {
+	    0, 31, 61, 92, 122, 153, 184, 214, 245, 275, 306, 337 };
+	day += monthdays[month];
+    }
+#endif
+    return (long)(year/4 - year/100 + year/400 + day) + year*365L - 719469L
+	   - 2922;
+}
+
 
 #define BASEYEAR	    1978
 #define DAYS_PER_YEAR	    365
@@ -74,7 +136,6 @@ word time;
     }
 
     {
-	ulong i, j, t;
 	int year, month, day;
 
 	if (date < DATE_MIN)
@@ -82,50 +143,58 @@ word time;
 
 	day = date & 31;
 	date >>= 5;
-	month = (date & 15) - 1;
+	month = (date & 15);
 	date >>= 4;
 	year = date + 1980;
 
-	if ((unsigned)month > 11 ||
+	if ((unsigned)month > 12 ||
 	    (unsigned)day > (unsigned)daycount[month]) {
 	    day = 31;
-	    month = 11;
+	    month = 12;
 	    year = 1979;
 	}
 
-	j = year - BASEYEAR;
+#if 1
+	datestamp->ds_Days = unixdays(year, month, day);
+#else
+	{
+	    ulong i, j, t;
+	    j = year - BASEYEAR;
 
-	/* Get the next lower full leap period (4 years and a day) since ... */
-	t = (year - BASEYEAR) & ~3;
-	i = t;
-	t = (t / 4) * (4 * DAYS_PER_YEAR + 1);
+	    /* Get the next lower full leap period (4 years and a day) since ... */
+	    t = (year - BASEYEAR) & ~3;
+	    i = t;
+	    t = (t / 4) * (4 * DAYS_PER_YEAR + 1);
 
-	/* t now is the number of days in 4 whole years since ... */
+	    /* t now is the number of days in 4 whole years since ... */
 
-	while (i < j) {
-	    t += DAYS_PER_YEAR;
-	    if (LeapYear(i + BASEYEAR)) {
-		t++;
+	    while (i < j) {
+		t += DAYS_PER_YEAR;
+		if (LeapYear(i + BASEYEAR)) {
+		    t++;
+		}
+		i++;
 	    }
-	    i++;
-	}
 
-	/* t now is the number of days in whole years since ... */
+	    /* t now is the number of days in whole years since ... */
 
-	for (i = 0; i < month; i++) {
-	    t += daycount[i];
-	    if (i == 1 && LeapYear(year)) {
-		t++;
+	    month--;	/* 1..12 -> 0..11 */
+	    for (i = 0; i < month; i++) {
+		t += daycount[i];
+		if (i == 1 && LeapYear(year)) {
+		    t++;
+		}
 	    }
+
+	    /* t now is the number of days in whole months since ... */
+
+	    t += day - 1;
+
+	    /* t now is the number of days in whole days since ... */
+
+	    datestamp->ds_Days = t;
 	}
-
-	/* t now is the number of days in whole months since ... */
-
-	t += day - 1;
-
-	/* t now is the number of days in whole days since ... */
-
-	datestamp->ds_Days = t;
+#endif
     }
 }
 
