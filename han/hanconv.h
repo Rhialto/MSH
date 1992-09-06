d92a81b1aa1111fa40a5326f153dc270bf6f8e3e
Rhialto <rhialto@falu.nl>
1992-09-06 00:10:25 +0000

    Clean up declaration.

--- han/hanconv.h
+++ han/hanconv.h
@@ -1,23 +1,28 @@
 /*-
- * $Id: hanconv.h,v 1.45 91/10/03 23:35:59 Rhialto Exp $
+ * $Id: hanconv.h,v 1.46 91/10/06 18:25:14 Rhialto Rel $
  * $Log:	hanconv.h,v $
+ * Revision 1.46  91/10/06  18:25:14  Rhialto
+ *
+ * Freeze for MAXON
+ *
  * Revision 1.45  91/10/03  23:35:59  Rhialto
  * Initial version
- * 
+ *
  *
  * HANCONV.H
  *
- * This code is (C) Copyright 1991 by Olaf Seibert. All rights reserved.
+ * This code is (C) Copyright 1991-1992 by Olaf Seibert. All rights reserved.
  * May not be used or copied without a licence.
 -*/
 
 #ifdef CONVERSIONS
 
 typedef void (*ConversionFunc)(byte *from, byte *to, long fromsize);
-extern ConversionFunc rd_Conv[];
-extern ConversionFunc wr_Conv[];
+extern const ConversionFunc rd_Conv[];
+extern const ConversionFunc wr_Conv[];
 
 enum Conversion {
+    ConvUnspecified = -1,
     ConvNone, ConvPCASCII, ConvSTASCII,
     ConvFence
 };
